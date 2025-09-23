4b550f6935daab15297792e7dc80136449797154
cerf007 <cerf02062007@gmail.com>
2025-09-22 19:54:20 -0600

    Ejercicio 10, Ejercicio de aumentar la puntuacion con variable global

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,34 @@
+﻿//10.Juego de puntuación con variables globales
+//	Declara una variable global puntuacion. Cada vez que se llame al método GanarPuntos, la puntuación debe aumentar en 10 y mostrarse en pantalla.
+
+using System;
+
+class Program;
+{
+    static int puntuacion = 0;
+    static void Main()
+    {
+       bool seguir = true;
+       while (!seguir)
+       {
+            Console.WriteLine("Ingrese la siguientes opciones");
+            Console.WriteLine("1. Aumentar Puntucación");
+            Console.WriteLine("2. Salir del programa");
+            int opcion = (int.TryParse(Console.ReadLine(), out opcion)
+                {
+
+            }
+
+            switch(opcion)
+            {
+                case 1:
+                    GanarPuntos()
+                case 2: GanarPuntos()
+                default
+
+            }
+
+       }
+    }
+    
+}
